b7da726c4ff6bf6e0b787e766c9303251508d6dc
Abir <rahmanmushfiq94@gmail.com>
2020-02-11 15:54:30 +0600

    practice to development

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,url_for,jsonify,request,json
-# this is practice branch
+# from practice to development
 from models.condition import Condition
 from models.user import User
 # from dbconnect import connection
